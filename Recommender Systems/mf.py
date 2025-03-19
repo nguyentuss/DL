349e044f394e1245f67dd068b02ef5f7bffe7544
nguyentuss <nttu.93tn@gmail.com>
2025-03-19 19:53:50 +0700

    Update

--- Recommender Systems/mf.py
+++ Recommender Systems/mf.py
@@ -1,18 +1,26 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import time
+from torch.utils.data import DataLoader, TensorDataset
 
-class MatrixFactorization(nn.Module):
-    def __init__(self, num_users, num_items, num_factors=30):
-        super().__init__
+class MF(nn.Module):
+    def __init__(self, num_users, num_items, num_factors=30, device):
+        super(MF, self).__init__()
+
+        self.device = device
         self.user_factors = nn.Embedding(num_users, num_factors)
         self.item_factors = nn.Embedding(num_items, num_factors)
-        self.user_bias = nn.Embedding(num_items, 1)
-        self.item_bias = nn.Embedding(num_items, 1)
+        self.user_bias = nn.Embedding(num_items, 1).squeeze()
+        self.item_bias = nn.Embedding(num_items, 1).squeeze()
 
         nn.init.normal_(self.user_factors.weight, std=0.01)
         nn.init.normal_(self.item_factors.weight, std=0.01)
         nn.init.zeros_(self.item_bias.weight)
         nn.init.zeros_(self.item_factors.weight)
+        self.to(device)
+    
     def forward(self, user_id, item_id):
         P_u = self.user_factors(user_id)  # User latent vector
         Q_i = self.item_factors(item_id)  # Item latent vector
@@ -20,6 +28,14 @@ class MatrixFactorization(nn.Module):
         b_i = self.item_bias(item_id).squeeze()  # Item bias
         outputs = (P_u * Q_i).sum(dim=1) + b_u + b_i
         return outputs
-    def rmse(preds, targets):
-        return torch.sqrt(torch.mean((preds-targets)**2))
-    def evaluate(model, test_load, device)
+    
+    def evaluator(self, test_loader):
+        self.eval()
+        criterion = nn.MSELoss()
+        with torch.no_grad():
+            for users, items, ratings in test_loader:
+                users, items, ratings = users.to(self.device), items.to(self.device), ratings.float().to(self.device)
+                preds = self(users, items)
+                loss = criterion(preds, ratings.float())
+                total_loss += loss.item() * len(ratings)
+                
